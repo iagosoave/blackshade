4e0112684613f874226832f74b0ae58f3f9ff30c
iagosoave <iago.soave@gmail.com>
2025-08-19 00:49:56 -0300

    up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,24 @@ import BackgroundVideo from "../components/BackgroundVideo";
 // Importe todos os vídeos da home que você deseja
 import backgroundVideo1 from "../01.mp4";
 import backgroundVideo2 from "../02.mp4";
+import backgroundVideo3 from "../03.mp4";
+import backgroundVideo4 from "../04.mp4";
+import backgroundVideo5 from "../05.mp4";
+import backgroundVideo6 from "../06.mp4";
+import backgroundVideo7 from "../07.mp4";
+import backgroundVideo8 from "../08.mp4";
 
 export default function HomePage() {
-  const videos = [backgroundVideo1, backgroundVideo2];
+  const videos = [
+    backgroundVideo1,
+    backgroundVideo2,
+    backgroundVideo3,
+    backgroundVideo4,
+    backgroundVideo5,
+    backgroundVideo6,
+    backgroundVideo7,
+    backgroundVideo8
+  ];
 
   return (
     <motion.div
